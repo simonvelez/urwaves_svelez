3d58cd6a71775cf50efe406733152e7daeb4f0f9
Simón Vélez <145301431+simonvelez@users.noreply.github.com>
2023-09-26 20:33:05 -0500

    Add files via upload

--- URWAVES_aproximador derivadas/ec_transporte_matriz.m
+++ URWAVES_aproximador derivadas/ec_transporte_matriz.m
@@ -7,13 +7,12 @@ dt = 0.005;
 dx = 0.005;
 c = -0.5;
 tf = 0.3; % Tiempo final
-
+X = 0:dx:elem;
+Y = (0.4).*exp(-300.*(X-(0.5)).^2);
 
 % Yi = f(xi)
-X = 0:dx:elem;
 n = size(X);
 n = n(2);
-Y = func(X); %func está en func.m
 
 % Creación Matriz que al multiplicar con Yi = f(xi) es f'(xi)
 A = zeros(n,n);
@@ -27,7 +26,7 @@ Z = A * Y.';
 
 % U = matriz de u(x, t+dt) el "futuro" de la ecuación de transporte
 U = zeros(tf/dt+1, elem/dx+1);
-U(1,:) = func(X); %Llenar la primera fila con func(x), para después hacer el resta
+U(1,:) = (0.4).*exp(-300.*(X-(0.5)).^2); %Llenar la primera fila con func(x), para después hacer el resta
 
 % Implementación con multiplicación de matrices
 for i = 2:tf/dt
